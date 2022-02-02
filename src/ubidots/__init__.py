3a669149618bd90e982b7d84dc953c4ada5901d2
Alejandro Rosso <alejandro@ubidots.com>
2022-02-02 18:07:35 -0500

    Add module for configuration values

--- src/ubidots/__init__.py
+++ src/ubidots/__init__.py
@@ -1,6 +1,3 @@
-api_ver = "v2.0"
-base_url = "https://industrial.api.ubidots.com"
-token = ""
-
-from src.ubidots.error import *
-from src.ubidots.resources import *
+from . import config
+from .error import *
+from .resources import *
---
+++ src/ubidots/config.py
@@ -0,0 +1,3 @@
+api_ver = "v2.0"
+base_url = "https://industrial.api.ubidots.com"
+token = ""
