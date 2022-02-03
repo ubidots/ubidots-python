837387e8734222c22a43f14ceac6dde8aa960d2a
Alejandro Rosso <alejandro@ubidots.com>
2022-02-03 16:52:10 -0500

    Add device faker

---
+++ tests/api_resources/conftest.py
@@ -0,0 +1,43 @@
+from urllib.parse import urljoin
+
+import pytest
+
+from ubidots.api_client import _api_url
+
+DEFAULT_FIELDS = ["url", "id", "label", "name", "createdAt"]
+
+
+@pytest.fixture
+def fake_device(faker):
+    def _fake_device(fields: list = [], **kwargs):
+        device_id = faker.unique.hexify(text="".join(["^"] * 24))
+        device = {
+            "url": urljoin(_api_url, f"devices/{device_id}"),
+            "id": device_id,
+            "organization": None,
+            "label": faker.unique.slug(),
+            "name": " ".join(faker.words(nb=faker.random_int(min=0, max=4))),
+            "description": faker.sentence(
+                nb_words=faker.random_int(min=0, max=8)
+            ),
+            "tags": [],
+            "properties": {},
+            "isActive": True,
+            "lastActivity": 0,
+            "createdAt": "",
+            "variables": urljoin(_api_url, f"devices/{device_id}/variables"),
+            "variablesCount": 0,
+        }
+        for key, val in kwargs.items():
+            device[key] = val
+        return {k: v for k, v in device.items() if not fields or k in fields}
+
+    return _fake_device
+
+
+@pytest.fixture
+def fake_devices(faker, fake_device):
+    def _fake_devices(count: int = 1, fields: list = [], **kwargs):
+        return [fake_device(fields, **kwargs) for _ in range(count)]
+
+    return _fake_devices
