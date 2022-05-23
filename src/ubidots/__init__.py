d2927344da7ec7958ba30079d90b73d2d4ea2e81
Alejandro Rosso <alejandro@ubidots.com>
2022-05-23 16:17:48 -0500

    Define usage parameters.

--- src/ubidots/__init__.py
+++ src/ubidots/__init__.py
@@ -1,3 +1,6 @@
-from . import config
-from .api_resources import *
-from .error import *
+api_ver = "v2.0"
+base_url = "https://industrial.api.ubidots.com"
+token = ""
+
+from ubidots.api_resources import *
+from ubidots.error import *
