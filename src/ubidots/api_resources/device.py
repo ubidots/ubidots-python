3f842d4d23066b74faba4b4c351a7a8e47467445
Alejandro Rosso <alejandro@ubidots.com>
2022-05-23 17:30:41 -0500

    Add list() and fix retrieve() methods.

--- src/ubidots/api_resources/device.py
+++ src/ubidots/api_resources/device.py
@@ -1,8 +1,13 @@
-from ..mixins import ApiResourceMixin, RequestsMixin
+from ubidots.mixins import ApiResourceMixin, RequestsMixin
 
 
 class Device(RequestsMixin):
     _plural = "devices"
+
+    @classmethod
+    def list(cls, **filters):
+        devices = cls.get(cls._plural, filters).get("results", [])
+        return [ApiResourceMixin(device) for device in devices]
 
     @classmethod
     def create(cls, **props):
@@ -22,16 +27,16 @@ class Device(RequestsMixin):
         return ApiResourceMixin(device)
 
     @classmethod
-    def retrieve(cls, **props):
-        key = cls._entity_key(props.get("id"), props.get("label"))
-        device = cls.get(f"{cls._plural}/{key}", props)
+    def retrieve(cls, id: str = "", label: str = "", fields: list = []):
+        key = cls._entity_key(id, label)
+        device = cls.get(f"{cls._plural}/{key}", fields)
         return ApiResourceMixin(device)
 
     @classmethod
     def delete(cls, id: str = "", label: str = ""):
-        key = cls._entity_key(props.get("id"), props.get("label"))
+        key = cls._entity_key(id, label)
         cls.delete(f"{cls._plural}/{key}")
 
     @staticmethod
-    def _entity_key(id: str = None, label: str = None):
-        return id if id is not None else "~" + label
+    def _entity_key(id: str = "", label: str = ""):
+        return id if id else "~" + label
