e1379889b6aff3e4b8d8a2e11b02823b78a7b1eb
Alejandro Rosso <alejandro@ubidots.com>
2022-07-18 09:43:51 -0500

    Spread entity properties.

--- src/ubidots/api_resources/device.py
+++ src/ubidots/api_resources/device.py
@@ -1,3 +1,4 @@
+from ubidots.api_resources.variable import Variables
 from ubidots.mixins import ApiResourceMixin, RequestsMixin
 
 
@@ -7,35 +8,38 @@ class Devices(RequestsMixin):
     @classmethod
     def list(cls, **filters):
         devices = cls.get(cls._plural, filters).get("results", [])
-        return [ApiResourceMixin(device) for device in devices]
+        return [
+            ApiResourceMixin(**device, Variables=Variables)
+            for device in devices
+        ]
 
     @classmethod
     def new(cls, **props):
-        device = ApiResourceMixin(props)
+        device = ApiResourceMixin(**props, Variables=Variables)
         return device
 
     @classmethod
     def create(cls, **props):
         device = cls.post(f"{cls._plural}", props)
-        return ApiResourceMixin(device)
+        return ApiResourceMixin(**device, Variables=Variables)
 
     @classmethod
     def modify(cls, **props):
         key = cls._entity_key(props.get("id"), props.get("label"))
         device = cls.patch(f"{cls._plural}/{key}", props)
-        return ApiResourceMixin(device)
+        return ApiResourceMixin(**device, Variables=Variables)
 
     @classmethod
     def update(cls, **props):
         key = cls._entity_key(props.get("id"), props.get("label"))
         device = cls.put(f"{cls._plural}/{key}", props)
-        return ApiResourceMixin(device)
+        return ApiResourceMixin(**device, Variables=Variables)
 
     @classmethod
     def retrieve(cls, id: str = "", label: str = "", fields: list = []):
         key = cls._entity_key(id, label)
         device = cls.get(f"{cls._plural}/{key}", fields)
-        return ApiResourceMixin(device)
+        return ApiResourceMixin(**device, Variables=Variables)
 
     @classmethod
     def delete(cls, id: str = "", label: str = ""):
--- src/ubidots/api_resources/variable.py
+++ src/ubidots/api_resources/variable.py
@@ -7,30 +7,30 @@ class Variables(RequestsMixin):
     @classmethod
     def list(cls, **filters):
         variables = cls.get(cls._plural, filters).get("results", [])
-        return [ApiResourceMixin(variable) for variable in variables]
+        return [ApiResourceMixin(**variable) for variable in variables]
 
     @classmethod
     def create(cls, **props):
         variable = cls.post(f"{cls._plural}", props)
-        return ApiResourceMixin(variable)
+        return ApiResourceMixin(**variable)
 
     @classmethod
     def modify(cls, **props):
         key = cls._entity_key(props.get("id"), props.get("label"))
         variable = cls.patch(f"{cls._plural}/{key}", props)
-        return ApiResourceMixin(variable)
+        return ApiResourceMixin(**variable)
 
     @classmethod
     def update(cls, **props):
         key = cls._entity_key(props.get("id"), props.get("label"))
         variable = cls.put(f"{cls._plural}/{key}", props)
-        return ApiResourceMixin(variable)
+        return ApiResourceMixin(**variable)
 
     @classmethod
     def retrieve(cls, id: str = "", label: str = "", fields: list = []):
         key = cls._entity_key(id, label)
         variable = cls.get(f"{cls._plural}/{key}", fields)
-        return ApiResourceMixin(variable)
+        return ApiResourceMixin(**variable)
 
     @classmethod
     def delete(cls, id: str = "", label: str = ""):
