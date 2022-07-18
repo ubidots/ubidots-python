1cc01d0f4acee72154c9ef38781c5f1c79d29f4d
Alejandro Rosso <alejandro@ubidots.com>
2022-07-18 09:44:52 -0500

    Test send data to variable.

--- tests/test_data_api.py
+++ tests/test_data_api.py
@@ -21,108 +21,185 @@ def set_token(mocked_responses):
         assert call.request.headers.items() >= {"X-Auth-Token": TOKEN}.items()
 
 
-def test__send_single_variable_value(mocked_responses):
-    mocked_responses.add(
-        responses.POST,
-        f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/",
-        status=200,
-        json={"temperature": [{"status_code": 201}]},
-        match=[json_params_matcher({"temperature": 10})],
-    )
+class TestSendDataToDevice:
+    def test__send_single_variable_value(self, mocked_responses):
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device",
+            status=200,
+            json={"temperature": [{"status_code": 201}]},
+            match=[json_params_matcher({"temperature": 10})],
+        )
 
-    device = ubidots.Devices.new(label="test-device")
-    device.send_data(temperature=10)
+        device = ubidots.Devices.new(label="test-device")
+        device.send_data(temperature=10)
+
+    def test__send_multiple_variables_value(self, mocked_responses):
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device",
+            status=200,
+            json={
+                "temperature": [{"status_code": 201}],
+                "humidity": [{"status_code": 201}],
+                "pressure": [{"status_code": 201}],
+            },
+            match=[
+                json_params_matcher(
+                    {"temperature": 10, "humidity": 90, "pressure": 78}
+                )
+            ],
+        )
+
+        device = ubidots.Devices.new(label="test-device")
+        device.send_data(temperature=10, humidity=90, pressure=78)
+
+    def test__send_single_variable_dot(self, mocked_responses):
+        dot = {
+            "value": 10,
+            "timestamp": 1634311791000,
+            "context": {"status": "cold"},
+        }
+
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device",
+            status=200,
+            json={"temperature": [{"status_code": 201}]},
+            match=[json_params_matcher({"temperature": dot})],
+        )
+
+        device = ubidots.Devices.new(label="test-device")
+        device.send_data(temperature=dot)
+
+    def test__send_multiple_variables_dots(self, mocked_responses):
+        temperature = {
+            "value": 10,
+            "timestamp": 1634311791000,
+            "context": {"status": "cold"},
+        }
+        humidity = {
+            "value": 90,
+            "timestamp": 1634311791000,
+            "context": {"status": "High humidity"},
+        }
+        pressure = {
+            "value": 78,
+            "timestamp": 1634311791000,
+            "context": {"status": "Normal"},
+        }
+
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device",
+            status=200,
+            json={"temperature": [{"status_code": 201}]},
+            match=[
+                json_params_matcher(
+                    {
+                        "temperature": temperature,
+                        "humidity": humidity,
+                        "pressure": pressure,
+                    }
+                )
+            ],
+        )
+
+        device = ubidots.Devices.new(label="test-device")
+        device.send_data(
+            temperature=temperature, humidity=humidity, pressure=pressure
+        )
+
+    def test__send_position(self, mocked_responses):
+        position = {"value": 1, "context": {"lat": 6.5423, "lng": -70.5783}}
+
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device",
+            status=200,
+            json={"temperature": [{"status_code": 201}]},
+            match=[json_params_matcher({"position": position})],
+        )
+
+        device = ubidots.Devices.new(label="test-device")
+        device.send_data(position=position)
 
 
-def test__send_multiple_variables_value(mocked_responses):
-    mocked_responses.add(
-        responses.POST,
-        f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/",
-        status=200,
-        json={
-            "temperature": [{"status_code": 201}],
-            "humidity": [{"status_code": 201}],
-            "pressure": [{"status_code": 201}],
-        },
-        match=[
-            json_params_matcher(
-                {"temperature": 10, "humidity": 90, "pressure": 78}
-            )
-        ],
-    )
+class TestSendDataToVariable:
+    def test__send_single_variable_value(self, mocked_responses):
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/temperature/values",
+            status=200,
+            json={"status_code": 201},
+            match=[json_params_matcher({"value": 10})],
+        )
 
-    device = ubidots.Devices.new(label="test-device")
-    device.send_data(temperature=10, humidity=90, pressure=78)
+        device = ubidots.Devices.new(label="test-device")
+        variable = device.Variables.new(label="temperature")
+        variable.send_data(value=10)
 
+    def test__send_single_variable_dot(self, mocked_responses):
+        dot = {
+            "value": 10,
+            "timestamp": 1634311791000,
+            "context": {"status": "cold"},
+        }
 
-def test__send_single_variable_dot(mocked_responses):
-    dot = {
-        "value": 10,
-        "timestamp": 1634311791000,
-        "context": {"status": "cold"},
-    }
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/temperature/values",
+            status=200,
+            json={"status_code": 201},
+            match=[json_params_matcher(dot)],
+        )
 
-    mocked_responses.add(
-        responses.POST,
-        f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/",
-        status=200,
-        json={"temperature": [{"status_code": 201}]},
-        match=[json_params_matcher({"temperature": dot})],
-    )
+        device = ubidots.Devices.new(label="test-device")
+        variable = device.Variables.new(label="temperature")
+        variable.send_data(**dot)
 
-    device = ubidots.Devices.new(label="test-device")
-    device.send_data(temperature=dot)
+    def test__send_multiple_variable_dots(self, mocked_responses):
+        dots = [
+            {
+                "value": 10,
+                "timestamp": 1634311791000,
+                "context": {"status": "cold"},
+            },
+            {
+                "value": 12,
+                "timestamp": 1634311891000,
+                "context": {"status": "cold"},
+            },
+            {
+                "value": 14,
+                "timestamp": 1634311991000,
+                "context": {"status": "cold"},
+            },
+        ]
 
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/temperature/values",
+            status=200,
+            json=[{"status_code": 201} for _ in enumerate(dots)],
+            match=[json_params_matcher(dots)],
+        )
 
-def test__send_multiple_variables_dots(mocked_responses):
-    temperature = {
-        "value": 10,
-        "timestamp": 1634311791000,
-        "context": {"status": "cold"},
-    }
-    humidity = {
-        "value": 90,
-        "timestamp": 1634311791000,
-        "context": {"status": "High humidity"},
-    }
-    pressure = {
-        "value": 78,
-        "timestamp": 1634311791000,
-        "context": {"status": "Normal"},
-    }
+        device = ubidots.Devices.new(label="test-device")
+        variable = device.Variables.new(label="temperature")
+        variable.send_data(*dots)
 
-    mocked_responses.add(
-        responses.POST,
-        f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/",
-        status=200,
-        json={"temperature": [{"status_code": 201}]},
-        match=[
-            json_params_matcher(
-                {
-                    "temperature": temperature,
-                    "humidity": humidity,
-                    "pressure": pressure,
-                }
-            )
-        ],
-    )
+    def test__send_position(self, mocked_responses):
+        position = {"value": 1, "context": {"lat": 6.5423, "lng": -70.5783}}
 
-    device = ubidots.Devices.new(label="test-device")
-    device.send_data(
-        temperature=temperature, humidity=humidity, pressure=pressure
-    )
+        mocked_responses.add(
+            responses.POST,
+            f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/position/values",
+            status=200,
+            json={"status_code": 201},
+            match=[json_params_matcher(position)],
+        )
 
-
-def test__send_position(mocked_responses):
-    position = {"value": 1, "context": {"lat": 6.5423, "lng": -70.5783}}
-
-    mocked_responses.add(
-        responses.POST,
-        f"https://industrial.api.ubidots.com/api/v1.6/devices/test-device/",
-        status=200,
-        json={"temperature": [{"status_code": 201}]},
-        match=[json_params_matcher({"position": position})],
-    )
-
-    device = ubidots.Devices.new(label="test-device")
-    device.send_data(position=position)
+        device = ubidots.Devices.new(label="test-device")
+        variable = device.Variables.new(label="position")
+        variable.send_data(**position)
